6a72c2e9f97c5b5c5e4a7eedfe78ffea7bc198a5
Daniel Toebe <dtoebe@gmail.com>
2016-03-11 08:17:45 -0700

    Commented all code

--- README.md
+++ README.md
@@ -3,6 +3,5 @@
 Watches the processes of the slideshow defined in settings.ini file then sends it to the webserver
 
 ### TODO
-* Code Comments
 * if socket server (slideshow-webserver) is not listening retry after n time
 * Clean Up logging
--- main.go
+++ main.go
@@ -1,3 +1,12 @@
+// Author: Daniel Toebe <dtoebe@gmail.com>
+//License: All code I have written is licensed under MIT
+//	All other code I have not written such as dependacies are licensed under thier
+//	respecive License
+
+//slideshow-process watcher: uses dbus to watch services in Systemd
+//systemd services defined in the settings.ini
+//this file will be used under root
+//settings.ini should be plces in the same dir as the compiled binary
 package main
 
 import (
@@ -14,15 +23,19 @@ import (
 	"github.com/vaughan0/go-ini"
 )
 
+//services is a struct used to structure the data to be sent to the webserver
 type services struct {
 	Service []service `json:"service"`
 }
 
+//service is the structure of data each watched service to be sent to the webserver
 type service struct {
 	Name   string `json:"name"`
 	Status bool   `json:"status"`
 }
 
+//loadSettings loads the settings.ini file and returns the core amd images section
+//	as map[string]string
 func loadSettings() (core, services map[string]string) {
 	file, err := ini.LoadFile("./settings.ini")
 	if err != nil {
@@ -31,6 +44,8 @@ func loadSettings() (core, services map[string]string) {
 	return file["core"], file["services"]
 }
 
+//formatServiceSettings takes the val string in a map[string]string and seperates
+//	it at "," and returns it as a the map as a map[string][]string
 func formatServiceSettings(data map[string]string) map[string][]string {
 	services := make(map[string][]string)
 	for key, val := range data {
@@ -39,6 +54,7 @@ func formatServiceSettings(data map[string]string) map[string][]string {
 	return services
 }
 
+//startSystemConn starts watching dbus and returns its connection as a dbus.Conn leteral
 func startSystemConn() (conn *dbus.Conn) {
 	conn, err := dbus.NewSystemdConnection()
 	if err != nil {
@@ -47,6 +63,8 @@ func startSystemConn() (conn *dbus.Conn) {
 	return conn
 }
 
+//getUnits takes the dbus.Conn lteral ant collects the entire list of systemd services
+//	returns an array of []dbus.UnitStatus
 func getUnits(conn *dbus.Conn) (units []dbus.UnitStatus) {
 	units, err := conn.ListUnits()
 	if err != nil {
@@ -55,6 +73,9 @@ func getUnits(conn *dbus.Conn) (units []dbus.UnitStatus) {
 	return units
 }
 
+//getStatues takes the wanted services (map[string]string) and the list of services
+//	collected ([]dbus.UnitStatus) then returns the needed status ActiveState as
+//	map[string]string
 func getStatues(name map[string]string, units []dbus.UnitStatus) (names map[string]string) {
 	for _, unit := range units {
 		for key, _ := range name {
@@ -67,7 +88,13 @@ func getStatues(name map[string]string, units []dbus.UnitStatus) (names map[stri
 	return name
 }
 
+//startServiceWatcher takes the wanted services (map[string][]string) the connection (*dbus.Conn)
+//	and the core info from the settings file (map[string]string) starts serviceRoutine
+//	as a goroutine to activly checking the status of the wanted service, also
+//	starts an infinate loop and structures the returned data from serviceRoutine
+//	and sends it to the websever
 func startServiceWatcher(services map[string][]string, conn *dbus.Conn, core map[string]string) {
+	//NOTE: Does this func do to much?
 	status := make(chan map[string]string)
 	data := make(chan []byte)
 	duration, err := strconv.Atoi(core["check_duration"])
@@ -82,6 +109,9 @@ func startServiceWatcher(services map[string][]string, conn *dbus.Conn, core map
 	}
 }
 
+//serviceRoutine takes the wanted services (map[string][]string), chan for the state
+//	of the wanted services (chan map[string]string) and the delay to block the loop (int)
+//	in seconds
 func serviceRoutine(services map[string][]string, conn *dbus.Conn, servChan chan map[string]string, delay int) {
 	servStatus := make(map[string]string)
 	fmt.Println(services)
@@ -108,6 +138,8 @@ func serviceRoutine(services map[string][]string, conn *dbus.Conn, servChan chan
 	}
 }
 
+//socketClient simple tcp socket server that only sends data, not receive because
+//	this script runs as root user: takes the host and port (string) and data (<-chan []byte)
 func socketClient(host, port string, data <-chan []byte) {
 	conn, err := net.Dial("tcp", host+":"+port)
 	if err != nil {
@@ -118,6 +150,8 @@ func socketClient(host, port string, data <-chan []byte) {
 	defer conn.Close()
 }
 
+//parseMapToJSON takes the statuses of the wanted services and a data chan (chan []byte)
+//	converts it the services struct and json.Marshal and send out the []byte
 func parseMapToJSON(servStatus map[string]string, data chan []byte) {
 
 	aServ := make([]service, len(servStatus), len(servStatus))
